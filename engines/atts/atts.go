b501d3743b7dfde3b08eebd5b72a930957596d66
Kyle Ladd <kyle@ladd.io>
2015-07-19 13:06:41 -0400

    Fix comment mistakes in atts.go

--- engines/atts/atts.go
+++ engines/atts/atts.go
@@ -4,10 +4,10 @@ package atts
 
 import "os/exec"
 
-// Speaker is a driver using Apple's TTTS
+// Speaker implements tts.Speaker using Apple's TTS
 type Speaker struct{}
 
-// Say uses OS X builtin text to speech
+// Say uses OS X's built-in text to speech
 func (s *Speaker) Say(phrase string) {
 	exec.Command("say", phrase).Run()
 }
